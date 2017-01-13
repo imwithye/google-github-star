773c7a4d69891d389448d1d0652465c777714300
Ciel <imwithye@gmail.com>
2017-01-14 03:17:35 +0800

    show result urls

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var resultUrls = function() {
-  return Array.from(document.getElementsByClassName('rc')).map(function(div) {
-    return div.getElementsByClassName('r')[0].getElementsByTagName('a')[0].href;
-  })
+  var getParameterByName = function(name, url) {
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var results = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)").exec(url);
+    return (!results || !results[2])
+             ? ''
+             : decodeURIComponent(results[2].replace(/\+/g, " "));
+  };
+
+  return Array.from(document.getElementsByClassName('rc'))
+    .map(div => div.getElementsByClassName('r')[0].getElementsByTagName('a')[0])
+    .filter(a => a.host.toLowerCase().indexOf('github.com') >= 0 ||
+                 a.host.toLowerCase().indexOf('github.io') >= 0 ||
+                 getParameterByName("url", a.href).indexOf('github.com') >= 0 ||
+                 getParameterByName("url", a.href).indexOf('github.io') >= 0)
+    .map(a => a.host.toLowerCase().indexOf('github.com') >= 0 ||
+                  a.host.toLowerCase().indexOf('github.io') >= 0
+                ? a.href
+                : getParameterByName("url", a.href));
 };
